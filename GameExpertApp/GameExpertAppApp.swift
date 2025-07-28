c43885d2d82372d6322c746389bd20b7fe8807f1
Denny Octavian <dennyoctavian164@gmail.com>
2025-07-28 22:51:28 +0700

    Initial Commit

---
+++ GameExpertApp/GameExpertAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameExpertAppApp.swift
+//  GameExpertApp
+//
+//  Created by Denny Octavian on 28/07/25.
+//
+
+import SwiftUI
+
+@main
+struct GameExpertAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GameExpertAppTests/GameExpertAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  GameExpertAppTests.swift
+//  GameExpertAppTests
+//
+//  Created by Denny Octavian on 28/07/25.
+//
+
+import Testing
+@testable import GameExpertApp
+
+struct GameExpertAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
